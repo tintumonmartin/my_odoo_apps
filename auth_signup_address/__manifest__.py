15402664dce94964ed88e0b8efac5f3e2ee6e188
Tintumon M <tintumonmartin@gmail.com>
2019-01-27 14:23:56 +0530

    [ADD] Icon, Description page and License

--- auth_signup_address/__manifest__.py
+++ auth_signup_address/__manifest__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Signup with Address",
-    'summary': """Signup page with Address.""",
-    'description': """
-        Signup page with Address 
-            - New Customer can give address while signup.
+    'name': 'Signup with Address',
+    'summary': 'Signup page with Address.',
+    'description': """Signup page with Address 
+            - New Customer can give complete address with phone number while signup.
             - List of States and Countries will be selected.
+            - Need to enable signup in settings.
     """,
-    'author': "Tintumon .M",
-    'website': "http://www.tintumon.co.in",
-    'category': 'Uncategorized',
+    'author': 'Tintumon .M',
+    'website': 'http://www.tintumon.co.in',
+    'license': 'AGPL-3',
+    'category': 'Website',
     'version': '0.1',
     'depends': [
         'base',
