a3b656eb3cf9bdb816f0fe02cea85a579db1f083
Tintumon M <tintumonmartin@gmail.com>
2019-01-30 12:57:09 +0530

    [ADD] Cover image and re-structured folder

--- auth_signup_address/__manifest__.py
+++ auth_signup_address/__manifest__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Signup with Address',
-    'summary': 'Signup page with Address.',
+    'name': 'Signup with Address, Phone and Date of Birth',
+    'summary': 'Signup page with Address, Phone and Date of Birth Fields.',
     'description': '''Signup page with Address 
             - New Customer can give complete address with phone number while signup.
             - List of all States and Countries will be shown selected field in Signup page.
             - Add Date of Birth field.
             - You can choose whether Phone/Address/Date of Birth need to shown in Signup page in Configuration. 
-            Default will be true. 
+            Default will be enabled.
+            - Upload profile picture while sign up(Upcoming feature)
     ''',
     'author': 'Tintumon .M',
     'website': 'http://www.tintumon.co.in',
     'license': 'AGPL-3',
     'category': 'Website',
     'version': '0.1',
+    'images': ['static/description/banner.jpg'],
     'depends': [
         'base',
         'auth_signup',
